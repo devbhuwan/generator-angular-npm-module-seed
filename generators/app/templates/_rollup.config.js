afe6a79f245be42e37097b03c520d504b46e6deb
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-08-19 23:58:20 +0400

    setup

--- generators/app/templates/_rollup.config.js
+++ generators/app/templates/_rollup.config.js
@@ -1,6 +1,71 @@
+import fs from 'fs';
+import path from 'path';
+
+const outputFolder = "./dist";
+const sourceFolder = "src";
+
+function isIgnoreExtensions(source) {
+  let extension = path.extname(source);
+  return extension === '.ts' ||
+    extension === '.map' ||
+    extension === '.js';
+}
+
+function copyFileSync(source, target) {
+
+  let targetFile = target;
+
+  //if target is a directory a new file with the same name will be created
+  if (fs.existsSync(target)) {
+    if (fs.lstatSync(target).isDirectory()) {
+      targetFile = path.join(target, path.basename(source));
+    }
+  }
+  if (isIgnoreExtensions(source))
+    return;
+
+  console.log("Source=" + source + ",Target=" + target);
+  fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
+function copyFolderRecursiveSync(source, target, extension) {
+  let files = [];
+
+  //check if folder needs to be created or integrated
+  let targetFolder = path.join(target, path.basename(source)).replace(sourceFolder, '').replace('//', '/');
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder);
+  }
+
+  //copy
+  if (fs.lstatSync(source).isDirectory()) {
+    files = fs.readdirSync(source);
+    files.forEach(function (file) {
+      let curSource = path.join(source, file);
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyFolderRecursiveSync(curSource, targetFolder);
+      } else {
+        copyFileSync(curSource, targetFolder);
+      }
+    });
+  }
+}
+
+const copyPlugin = function (options) {
+  return {
+    ongenerate() {
+      const targDir = path.dirname(options.target);
+      if (!fs.existsSync(targDir)) {
+        fs.mkdirSync(targDir);
+      }
+      copyFolderRecursiveSync(options.src, options.target);
+    }
+  };
+};
+
 export default {
-	entry: 'dist/index.js',
-	dest: 'dist/bundles/<%= projectName %>.umd.js',
+  entry: outputFolder + '/index.js',
+  dest: outputFolder + '/bundles/<%= projectName %>.umd.js',
 	sourceMap: false,
 	format: 'umd',
 	moduleName: 'ng.<%= projectName %>',
@@ -13,5 +78,11 @@ export default {
 		'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
 		'rxjs/add/observable/fromEvent': 'Rx.Observable',
 		'rxjs/add/observable/of': 'Rx.Observable'
-	}
+	},
+  plugins: [
+    copyPlugin({
+      src: './' + sourceFolder,
+      target: outputFolder
+    })
+  ]
 }
